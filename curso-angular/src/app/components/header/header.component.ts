9f207197d18a1d67b009e24c14318d7e88ed7e0f
Leonardo Machado <leonardomachado.sjc@gmail.com>
2023-03-06 15:56:59 -0300

    Update header.component.ts

--- curso-angular/src/app/components/header/header.component.ts
+++ curso-angular/src/app/components/header/header.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  
+  ismenuOpen = false;
+  clickMenu(): void {
+    this.ismenuOpen = !this.ismenuOpen;
+  }
 }
